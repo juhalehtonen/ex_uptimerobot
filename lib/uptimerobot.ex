003a4badf2f350a657ed99e493b85341de6a16d1
Juha Lehtonen <juha.t.lehtonen.91@gmail.com>
2017-04-29 14:29:24 +0300

    Initial commit

---
+++ lib/uptimerobot.ex
@@ -0,0 +1,18 @@
+defmodule Uptimerobot do
+  @moduledoc """
+  Documentation for Uptimerobot.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Uptimerobot.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/uptimerobot_test.exs
@@ -0,0 +1,8 @@
+defmodule UptimerobotTest do
+  use ExUnit.Case
+  doctest Uptimerobot
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
