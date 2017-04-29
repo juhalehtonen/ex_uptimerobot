cdacf4bfa68c88fc950ff59408ff2b57527d5c1c
Juha Lehtonen <juha.t.lehtonen.91@gmail.com>
2017-04-29 16:43:02 +0300

    Add base for new monitor functions

--- lib/uptimerobot/monitor.ex
+++ lib/uptimerobot/monitor.ex
@@ -8,6 +8,9 @@ defmodule Uptimerobot.Monitor do
   - resetMonitor
   """
 
+
+  ## API PATHS
+
   @doc """
   Get data for all monitors.
   """
@@ -18,5 +21,31 @@ defmodule Uptimerobot.Monitor do
       {:error, reason} ->
         reason
     end
+  end
+
+  @doc """
+  Add a new monitor.
+  """
+  def new_monitor() do
+    nil
+  end
+
+
+  ## HELPERS & CONVENIENCE FUNCTIONS
+
+  @doc """
+  Get specific pieces of data from a get_monitors call.
+
+  Desired Data is passed as a list.
+  """
+  def get_from_monitors(data) when is_list(data) do
+    get_monitors()
+  end
+
+  @doc """
+  Check if a given URL is being monitored.
+  """
+  def is_monitored?(url) when is_binary(url) do
+    nil
   end
 end
