72fd842ee75701e0959bd44318a81168c60cc816
Juha Lehtonen <juha.t.lehtonen.91@gmail.com>
2017-05-09 21:51:53 +0300

    Adjust ExCoveralls and Travis configs

--- mix.exs
+++ mix.exs
@@ -9,7 +9,12 @@ defmodule ExUptimerobot.Mixfile do
      start_permanent: Mix.env == :prod,
      deps: deps(),
      test_coverage: [tool: ExCoveralls],
-     preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
+     preferred_cli_env: [
+       "coveralls": :test,
+       "coveralls.detail": :test,
+       "coveralls.post": :test,
+       "coveralls.html": :test
+     ],
      dialyzer: [plt_add_deps: :project],
      description: description(),
      package: package()]
