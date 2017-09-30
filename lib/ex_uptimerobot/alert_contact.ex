d72d8869fa8afceb08777cf54aaa8ae348aa5ec1
MichalDolata <dev@dolata.me>
2017-09-30 14:58:32 +0200

    Add newAlertContact

--- lib/ex_uptimerobot/alert_contact.ex
+++ lib/ex_uptimerobot/alert_contact.ex
@@ -26,4 +26,30 @@ defmodule ExUptimerobot.AlertContact do
       _                -> {:error, "Error getting alert contacts"}
     end
   end
+
+  @doc """
+  Add new alert contact with given parameters.
+
+  Two parameters are required: `type` and `value`.
+  One is optional: `friendly_name`
+
+  ## Example
+
+    iex>ExUptimerobot.AlertContact.new_alert_contact([friendly_name: "Main email", value: "your-email@example.com", type: 2])
+    {:ok, response}
+  """
+  @spec new_alert_contact([tuple]) :: tuple
+  def new_alert_contact(params \\ [])
+  def new_alert_contact(params) when is_list(params) do
+    with {:ok, body} <- Request.post("newAlertContact", params),
+         {:ok, body} <- Parser.parse(body),
+         {:ok, resp} <- Request.response_status?(body)
+     do
+       {:ok, resp}
+     else
+       {:error, reason} -> {:error, reason}
+       _                -> {:error, "Error adding alert contact"}
+     end
+  end
+  def new_alert_contact(_params), do: {:error, "Params not a keyword list"}
 end
--- test/alert_contact_test.exs
+++ test/alert_contact_test.exs
@@ -8,4 +8,12 @@ defmodule ExUptimerobot.AlertContactTest do
   test "Calling get_alert_contacts() with no user-provided params succeeds" do
     assert {:ok, _body} = AlertContact.get_alert_contacts()
   end
+
+  test "Adding new alert contact with insufficient params fails" do
+    assert {:error, _reason} = AlertContact.new_alert_contact()
+  end
+
+  test "Adding new alert contact with params of wrong type fails" do
+    assert {:error, "Params not a keyword list"} = AlertContact.new_alert_contact("failure")
+  end
 end
